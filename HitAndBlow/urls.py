967bc5de92c1d87969daac20f9815a719c0d704e
OuchiGenki <ouchigenki2025@gmail.com>
2025-11-06 05:14:42 +0900

    Add Hit and Blow game

---
+++ HitAndBlow/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('play/', views.play, name='play'),
+    path('ranking/', views.ranking, name='ranking'),
+]
+
+app_name = "hitandblow"
