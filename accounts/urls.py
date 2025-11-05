c0df136cc6944b7fb141e68e9224d4e4fd570101
MobG0929 <supercoderman0929@gmail.com>
2025-11-06 05:14:42 +0900

    Add Hit and Blow game

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import Signup, Login, Logout
+
+urlpatterns = [
+    path('signup/', Signup.as_view(), name='signup'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+]
+
+app_name = 'accounts'
