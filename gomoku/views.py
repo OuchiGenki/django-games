7491eee9e7dc7e481c376cf0029b7c41918c9091
OuchiGenki <ouchigenki2025@gmail.com>
2025-11-12 01:30:30 +0900

    indexビューの作成

--- gomoku/views.py
+++ gomoku/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Game
 
-# Create your views here.
+def index(request):
+    games = Game.objects.filter(user=request.user, finished=False).order_by('-created_at').first()
+    context = {
+        'games': games,
+    }
+    return render(request, 'gomoku/index.html', context)
